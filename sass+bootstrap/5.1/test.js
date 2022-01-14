81a267d952fcf48622a7a1cbb8cb005b62449b25
erez Calderon <erezcal1@gmail.com>
2022-01-14 18:15:54 +0200

    delete

--- sass+bootstrap/5.1/test.js
+++
@@ -1,7 +0,0 @@
-
-let rand = Math.floor(Math.random() * 3);
-let r = Math.floor(Math.random() * 256);
-let g = Math.floor(Math.random() * 255);
-let b = Math.floor(Math.random() * 255);
-let elem = elems[rand];
-elem.style.backgroundColor = `rgb(${r},${g},${b})`;
